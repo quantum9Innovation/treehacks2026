363b0a9dd730f1c3dcce19054f76a71a61d84bc9
Ananth <ananth@functor.systems>
2026-02-14 23:10:31 -0800

    feat: better spring trajectory

--- lean/Trajectories.lean
+++ lean/Trajectories.lean
@@ -40,7 +40,7 @@ end Wave
 namespace Spring
 
 def tilt : Millimeters := 20
-def amplitude : Millimeters := 50
+def amplitude : Millimeters := 80
 def frequency : Hertz := 1.5
 def phaseShift : Float := 0
 def nSamples : Nat := 200
